ef06f8ced12b5254043242137591c1d164103535
Kevin Roper <kevin@kevinstrove.com>
2024-04-22 23:44:53 -0700

    Fixed bug in note save function.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,17 +17,19 @@ router.get('/notes', (req, res) => {
     return res.json(notes.getNotes());
 });
 
-// Post notes writes notes to DB
 router.post('/notes', (req, res) => {
-    notes.saveNote(req.body);
-    return res.json();
-  console.log("router.post(/notes");
-});
+    notes
+      .saveNote(req.body)
+      .then((note) => res.json(note))
+      .catch((err) => res.status(500).json(err));
+  });
+  
 
 // DELETE notes that equals req.params.id
 router.delete('/notes/:id', (req, res) => {
-    console.log("router.notes(/notes");
- 
-});
+    notes.removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch((err) => res.status(500).json(err));;
+ });
 
 module.exports = router;
